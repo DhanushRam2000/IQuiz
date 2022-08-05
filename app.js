e4d7d3351f3b9c44ff9729ec41663147f4d3de29
ram <adhanushram2000@gmail.com>
2022-08-05 12:05:01 +0530

    Base project code

--- app.js
+++ app.js
@@ -12,95 +12,89 @@ const compression = require("compression");
 const Routes = require("./routes/route");
 const user = require("./models/user");
 
-// const uri = "mongodb://localhost:27017/IQUIZA2"; 
-// console.log(process.env.PORT);
-
-//online cluster
-const uri = `mongodb+srv://ramapp:${process.env.MONGO_PASSWORD}@cluster0-yce8w.mongodb.net/IQUIZA2?retryWrites=true&w=majority`; //?retryWrites=true&w=majority
+const uri = "mongodb://localhost:27017/IQUIZA2";
 
 app = express();
 
 const store = new sessionStore({
-    uri: uri,
-    collection: 'sessions'
-  });
+  uri: uri,
+  collection: 'sessions'
+});
 
-const filefilter = (req, file, cb)=>{
-  if(file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || !req.body.sid)
-  {
-    cb(null , true);
+const filefilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg' || !req.body.sid) {
+    cb(null, true);
   }
-  else
-  {
-    cb(null , false);
+  else {
+    cb(null, false);
   }
 };
 
 const filestorage = multer.diskStorage({
-    destination: (req,file,cb) => {
-      cb(null,"public/img");
-    },
-    filename: (req,file,cb) => {
-      cb(null, req.body.sid + path.extname(file.originalname)); // filename
-    }
+  destination: (req, file, cb) => {
+    cb(null, "public/img");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.sid + path.extname(file.originalname)); // filename
+  }
 });
 
 const csrfProtection = csrf();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 
-app.set("views","views");
+app.set("views", "views");
 
 app.use(compression());
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(morgan("common"));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(multer({storage:filestorage,fileFilter:filefilter}).single("qimg"));
+app.use(multer({ storage: filestorage, fileFilter: filefilter }).single("qimg"));
 
-app.use(favicon(path.join(__dirname,"public","img","favicon.ico")));
+app.use(favicon(path.join(__dirname, "public", "img", "favicon.ico")));
 
 app.use(Routes.public_routes);
 
 app.use(
-    session({
-      secret: 'added 5 packages from 5 contributors and audited 379 packages in 4.227s',
-      resave: false,
-      saveUninitialized: false,
-      store: store
-    })
+  session({
+    secret: 'added 5 packages from 5 contributors and audited 379 packages in 4.227s',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
 );
 
 app.use(csrfProtection);
 
 app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next();
-    }
-    user.findById(req.session.user._id)
-        .then(user => {
-        req.user = user;
-        next();
-        })
-        .catch(err => console.log(err));
+  if (!req.session.user) {
+    return next();
+  }
+  user.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
-    next();
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use(Routes.private_routes);
 
-app.use((req,res,next) => {
-    res.sendFile(path.join(__dirname,"views","error.html"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "views", "error.html"));
 });
 
-app.use((err,req,res,next) => {
+app.use((err, req, res, next) => {
   if (req.session) {
     req.session.destroy(err => {
       console.log(err);
@@ -111,7 +105,7 @@ app.use((err,req,res,next) => {
 })
 
 mongoose
-  .connect(uri,{ useUnifiedTopology: true ,  useNewUrlParser: true })
+  .connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(result => {
     console.log("Server started");
     app.listen(process.env.PORT || 3000);
